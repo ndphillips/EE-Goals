2dc304b573d25af2f3adc8447b9bacba87440ed6
Your Named <you@example.com>
2017-03-27 15:41:37 +0200

    Added file structure

---
+++ R/02_issue2.R
@@ -0,0 +1,11 @@
+# ---------------------
+# issue #2: Calculate, and plot, the probability of selecting the RSF best option at each trial
+# ---------------------
+
+
+# - Create a plot showing the proportion of participants that select the risk sensitive foraging
+#   (RSF) best option on each trial.
+# - Compare this to a plot showing the probability of selecting the high EV option
+# 
+# This could answer Rui's question: Are people more likely to select the RSF option as they 
+# get closer to the goal?
