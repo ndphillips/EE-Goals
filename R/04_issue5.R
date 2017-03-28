ac1e029b344270acfc5e3d5d6368ff9f5e528029
Your Named <you@example.com>
2017-03-28 15:14:33 +0200

    Updated R files 02_issue2 and 04_issue5

--- R/04_issue5.R
+++ R/04_issue5.R
@@ -4,3 +4,33 @@
 
 # Do variables such as points earned, proportion of reaching the goal, switch rates
 # (etc.) change consistently across games from game 1 to game 5? 
+
+library(yarrr)
+
+df.games <- readRDS("data/dataGameLevel.rds")
+
+# create separate variables for Environment and Goal
+df.games$Variance <- ifelse(df.games$condition %in% c(1,3), "low", "high")
+df.games$Goal <- ifelse(df.games$condition %in% c(1,2), "no", "yes")
+
+
+# do points earned differ over games?
+pirateplot(outcome.sum ~ game + Goal + Variance, data = df.games, ylab = "total points earned")
+
+# do mean switch rates differ over games?
+pirateplot(mean.switched ~ game + Goal + Variance, data = df.games, ylab = "mean switch rates")
+
+# do median response times differ over games?
+pirateplot(resp.time.median ~ game + Goal + Variance, data = df.games, ylab = "median response times")
+
+# do mean high EV chosen differ over games?
+pirateplot(highEV.mean ~ game + Goal + Variance, data = df.games, ylab = "mean high EV chosen")
+
+# do mean mean high variance chosen differ over games?
+pirateplot(high.var.chosen.mean ~ game + Goal + Variance, data = df.games, ylab = "mean high variance chosen")
+
+# differences in goal reached rates?
+aggregate(goalReached ~ game + Variance, FUN = mean, data = df.games)
+
+
+
